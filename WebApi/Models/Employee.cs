9ad0e8497850fecc736de5195fceaa4f3cb3da01
Roberto Macedo <robertomacedo3007@gmail.com>
2023-03-31 22:02:19 -0300

    Add project files.

---
+++ WebApi/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+    }
+}
---
+++ WebApi/Models/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+
+
+namespace WebApi.Controllers
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
